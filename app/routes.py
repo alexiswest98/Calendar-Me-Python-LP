087f4ecafb1892779613d14555d7a29997af5761
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-30 13:09:49 -0600

    Connect appointments to frontend

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,26 @@
 from flask import Blueprint, render_template
-
+import os
+import sqlite3
+from datetime import datetime
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+DB_FILE = os.environ.get("DB_FILE")
+
+
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        conn.row_factory = sqlite3.Row
+        curs = conn.cursor()
+        curs.execute("SELECT * FROM appointments")
+        appointments = [dict(row) for row in curs.fetchall()]
+        for appointment in appointments:
 
+            appointment["start_datetime"] = datetime.strptime(
+                appointment["start_datetime"], '%Y-%m-%d %H:%M:%S').strftime("%H:%M")
 
+            appointment["end_datetime"] = datetime.strptime(
+                appointment["end_datetime"], '%Y-%m-%d %H:%M:%S').strftime("%H:%M")
+
+        return render_template("main.html", appointments=appointments)
